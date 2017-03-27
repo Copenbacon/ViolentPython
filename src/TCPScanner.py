5ba8e4c125bb3df801ceadd69e428ea4d541a502
copenbacon <sclary50@gmail.com>
2017-03-27 00:56:25 -0700

    docstrings

--- src/TCPScanner.py
+++ src/TCPScanner.py
@@ -1,20 +1,24 @@
 import optparse
 from socket import *
 
-def connscan(tgtHost, tgtPort):
+
+def connscan(tgt_host, tgt_port):
+    """Attempt to create a connection to the target host and port."""
     try:
         connsckt = socket(AF_INET, SOCK_STREAM)
-        connsckt.connect((tgtHost, tgtPort))
-        print('[+]%d/tcp open' % tgtPort)
+        connsckt.connect((tgt_host, tgt_port))
+        print('[+]%d/tcp open' % tgt_port)
         connsckt.close()
     except:
-        print('[-]%d/tcp closed' % tgtPort)
+        print('[-]%d/tcp closed' % tgt_port)
 
-def portscan(tgtHost, tgtPorts):
+
+def portscan(tgt_host, tgt_ports):
+    """Attempt to resolve an IP Address to a friendly hostname, then print hostname or IP address."""
     try:
-        tgtIP = gethostbyname(tgtHost)
+        tgtIP = gethostbyname(tgt_host)
     except:
-        print("[-] Cannot resolve '%s': Unknown host" % tgtHost)
+        print("[-] Cannot resolve '%s': Unknown host" % tgt_host)
         return
     try:
         tgtName = gethostbyaddr(tgtIP)
@@ -22,19 +26,19 @@ def portscan(tgtHost, tgtPorts):
     except:
         print('\n[+] Scan Results for: ' + tgtIP)
     setdefaulttimeout(1)
-    for tgtPort in tgtPorts:
-        print('Scanning port ' + tgtPort)
-        connscan(tgtHost, int(tgtPort))
+    for tgt_port in tgt_ports:
+        print('Scanning port ' + tgt_port)
+        connscan(tgt_host, int(tgt_port))
 
 parser = optparse.OptionParser(
     'usage %prog -H' + '<target host> -p <target port>')
-parser.add_option('-H', dest='tgtHost', type='string',
+parser.add_option('-H', dest='tgt_host', type='string',
                   help='specify target host')
-parser.add_option('-p', dest='tgtPort', type='int',
+parser.add_option('-p', dest='tgt_port', type='int',
                   help='specify target port')
 (options, args) = parser.parse_args()
-tgtHost = options.tgtHost
-tgtPort = options.tgtPort
-if (tgtHost is None) or (tgtPort is None):
+tgt_host = options.tgt_host
+tgt_port = options.tgt_port
+if (tgt_host is None) or (tgt_port is None):
     print(parser.usage)
     exit(0)
