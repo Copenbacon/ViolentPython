32eb2ca88f173263b3b427b165dce87f13798083
copenbacon <sclary50@gmail.com>
2017-03-27 00:50:18 -0700

    port scanning

--- src/TCPScanner.py
+++ src/TCPScanner.py
@@ -1,4 +1,31 @@
 import optparse
+from socket import *
+
+def connscan(tgtHost, tgtPort):
+    try:
+        connsckt = socket(AF_INET, SOCK_STREAM)
+        connsckt.connect((tgtHost, tgtPort))
+        print('[+]%d/tcp open' % tgtPort)
+        connsckt.close()
+    except:
+        print('[-]%d/tcp closed' % tgtPort)
+
+def portscan(tgtHost, tgtPorts):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print("[-] Cannot resolve '%s': Unknown host" % tgtHost)
+        return
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print('\n[+] Scan Results for: ' + tgtName[0])
+    except:
+        print('\n[+] Scan Results for: ' + tgtIP)
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        print('Scanning port ' + tgtPort)
+        connscan(tgtHost, int(tgtPort))
+
 parser = optparse.OptionParser(
     'usage %prog -H' + '<target host> -p <target port>')
 parser.add_option('-H', dest='tgtHost', type='string',
